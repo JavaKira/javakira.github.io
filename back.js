17c373efef2b5e5dff2d94c3f9c0af12dfaee678
Javak <javakira@yandex.ru>
2024-11-21 15:35:50 +0300

    fd

--- back.js
+++ back.js
@@ -1,6 +1,6 @@
 var BackButton = window.Telegram.WebApp.BackButton;
 BackButton.show();
 window.Telegram.WebApp.onEvent('backButtonClicked', () => {
-    window.Telegram.WebApp.showAlert("Нет пути назад!");
+    location.href = "/"
     BackButton.hide();
 })
